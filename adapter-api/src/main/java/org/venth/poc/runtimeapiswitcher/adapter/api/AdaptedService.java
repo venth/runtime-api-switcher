67f9b18d6463f0ca5037f555ed0b858772ff5ad0
venth <artur.krysiak.warszawa@gmail.com>
2015-05-01 22:11:21 +0200

    introduced adapter api

---
+++ adapter-api/src/main/java/org/venth/poc/runtimeapiswitcher/adapter/api/AdaptedService.java
@@ -0,0 +1,10 @@
+package org.venth.poc.runtimeapiswitcher.adapter.api;
+
+/**
+ * @author Venth on 01/05/2015
+ */
+public interface AdaptedService {
+    String version();
+
+    String feature();
+}
--- webapp/src/main/java/org/venth/poc/runtimeapiswitcher/webapp/endpoint/ApplicationResource.java
+++ webapp/src/main/java/org/venth/poc/runtimeapiswitcher/webapp/endpoint/ApplicationResource.java
@@ -1,6 +1,7 @@
 package org.venth.poc.runtimeapiswitcher.webapp.endpoint;
 
 import org.springframework.stereotype.Component;
+import org.venth.poc.runtimeapiswitcher.adapter.api.AdaptedService;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
@@ -8,6 +9,10 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+import java.util.concurrent.atomic.AtomicReference;
 
 /**
  * @author Venth
@@ -16,24 +21,42 @@ import javax.ws.rs.core.MediaType;
 @Path("/application-service")
 public class ApplicationResource {
 
+    private Map<String, AdaptedService> services = new HashMap<>();
+    private AdaptedService version_unknown = new AdaptedService() {
+
+        @Override
+        public String version() {
+            return "unknown version";
+        }
+
+        @Override
+        public String feature() {
+            return "wrong handling adapter";
+        }
+    };
+
+    private AtomicReference<AdaptedService> activeService = new AtomicReference<>(version_unknown);
+
+
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     @Path("/version")
     public String getVersion() {
-        return "unknown version";
+        return activeService.get().version();
     }
 
     @PUT
     @Consumes(MediaType.TEXT_PLAIN)
     @Path("/version")
     public void switchAdapterVersionTo(String version) {
-
+        Optional<AdaptedService> adaptedService = Optional.ofNullable(services.get(version));
+        activeService.set(adaptedService.orElse(version_unknown));
     }
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     @Path("/feature")
     public String handleFeatureByAdapter() {
-        return "";
+        return activeService.get().feature();
     }
 }
